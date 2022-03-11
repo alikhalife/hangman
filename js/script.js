f98cc7e6c78adab13b86bd62a55e771b11ef9d2b
Ali Khalife <alikhalife05@gmail.com>
2022-03-11 14:00:15 +0100

    adding buttons for letters

--- js/script.js
+++ js/script.js
@@ -7,21 +7,29 @@
 
 
 
-var words = ["blue", "red", "yellow", "green"] // creating an array of colors
-var word = words[Math.floor(Math.random() * words.length)] // creating a var that will randomly select one element of my variable
-console.log(word)
+var randomwords = ["blue", "red", "yellow", "green"];// creating an array of colors
+var randomword = randomwords[Math.floor(Math.random() * randomwords.length)]; // creating a var that will randomly select one element of my variable
+console.log(randomword);
+
+var input = document.getElementById("input"); // creating input var for user input
+let seenLetters = [];
+let correctLetters = new Array(randomword.length) ;
+let letters;
 
 var lives = 10;
 document.getElementById("lives").innerHTML = "You have " + lives + " lives";
 
-var input = document.getElementById("input") // creating input var for user input
-let seenLetters = [];
-let correctLetters = [];
+var hiddenWord = document.getElementById("word");
+hiddenWord.innerHTML = ("Guess the word: " + correctLetters);
 
 
+console.log("Here, correctLetters should be an empty array: " + correctLetters)
 
-document.getElementById("btn").addEventListener("click", () => {
+
+document.getElementById("btn").addEventListener("click", (event) => { // c'est ça qu'il faut changer
     var inputValue = input.value; // store/get value of input user and store it in var inputValue
+    console.log(event.target.name)
+
 
     if (seenLetters.includes(inputValue)) {
         console.log("Type another letter");
@@ -29,24 +37,34 @@ document.getElementById("btn").addEventListener("click", () => {
     }
 
     else {
-        seenLetters.push(inputValue);
+        seenLetters.push(divLetters);//inputValue);
         console.log("seenLetters of new letters", seenLetters);
 
-        if (word.includes(inputValue)) {
-            let indicesOfLetter = indicesOfChar(word, inputValue) // get index of inputValue in word
-            indicesOfLetter.forEach(function(index) {
-                correctLetters[index] = inputValue 
+        if (randomword.includes(divLetters)) {
+            let indicesOfLetter = indicesOfChar(randomword, divLetters) // get index of inputValue in word
+            indicesOfLetter.forEach(function (index) {
+                correctLetters[index] = divLetters //inputValue
             })
-            
+
             // set inputValue in correctLetters at index of inputValue in word
             console.log("Ordered correctLetters", correctLetters);
+            
+            console.log("Here, correctLetters should be a filled array: " + correctLetters);
+            hiddenWord.innerHTML = ("Guess the word: " + correctLetters);
+           word =  myword(correctLetters);
+           console.log("This is zi " + word)
+
+
         }
         else {
-            console.log("sorry bro you lost a life") // counter should be here
+            console.log("sorry bro you lost a life")
+            lives-- // counter should be here
+            console.log(lives)
+            document.getElementById("lives").innerHTML = "You have " + lives + " lives left.";
         }
 
     }
-
+    
 
 })
 
@@ -61,3 +79,71 @@ function indicesOfChar(word, char) {
     }
     return arrayOfIndices;
 }
+    
+
+
+// creating a function that will store correctLetters and show it on browser
+// prendre un array et rendre du html soit un string 
+function myword(correctLetters) {
+    word = document.getElementById("word")
+    var divWord = document.createElement("div"); // creer un div 
+    divWord.style.display = "flex";
+    divWord.style.alignContent = "space-around";
+    divWord.style.justifyContent ="center";
+
+    for (let i = 0; i < correctLetters.length; i++) {
+        divWord.id = "divWord"
+        var divLetter = document.createElement("div")
+        correctLetters[i] = " _ "
+        divLetter.innerHTML = correctLetters[i];
+       
+        // console.log("CorrectLetters Length: " + correctLetters.length)
+        // console.log("divLetter's value: " + divLetter)
+
+        word.appendChild(divWord);
+        divWord.appendChild(divLetter);
+        
+        console.log("i: " + correctLetters[i])
+        console.log("this is divWord " + divWord)
+    }
+     // dans cette div qui va contenir les correctLetters
+    // pour ce faire, je vais iterer sur correctLetters (qui est un parametre)
+    // à chaque itération créer un div tag
+    // le contenu de ce div sera la lettre surlaquelle je me trouve lors de l'itération
+    // appendChild au premier div que j'ai créer avant de commencer de loop sur correctLetters
+    
+} 
+
+myword(correctLetters); //correctLetters doit être définit en fonction du length de mon random word
+
+
+
+
+
+// Creating letters buttons
+
+let divLetters = document.getElementById("letters")
+let div = document.createElement('div')
+
+div.id = "divLetter"
+div.innerHTML = letterButtons()
+divLetters.appendChild(div)
+console.log(divLetters)
+divLetters.addEventListener("click", (event) => {
+console.log(event.target.name)
+})
+
+function letterButtons () {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let letterBtn = "";
+    
+    for (let i = 0; i < alphabet.length; i++){
+        var letter = "<button class='letter' name=" + alphabet[i] +">" + alphabet[i] + "</button>";
+        letterBtn = letterBtn + letter;
+    }
+
+    return letterBtn;
+}
+
+
+
